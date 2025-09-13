37178b246c28e072ea55982503f33e0b915e14b5
GervinFung <gervinfungdaxuen@gmail.com>
2025-09-13 17:19:37 +0800

    refactor(order): improve type definition through tagged union

--- src/order/util.ts
+++ src/order/util.ts
@@ -1,65 +1,71 @@
 type OrderType = 'VIP' | 'Normal';
-type OrderState = 'PENDING' | 'PROCESSING' | 'COMPLETE';
 
-type Order = Readonly<{
+type OrderCommonProperties = Readonly<{
 	id: number;
 	type: OrderType;
-	state: OrderState;
-	assignedBotId?: undefined | number;
 }>;
+
+type PendingOrCompleteOrder = Readonly<
+	OrderCommonProperties & {
+		state: 'PENDING' | 'COMPLETE';
+	}
+>;
+
+type ProcessingOrder = Readonly<
+	OrderCommonProperties & {
+		state: 'PROCESSING';
+		assignedBotId: number;
+	}
+>;
+
+type Order = PendingOrCompleteOrder | ProcessingOrder;
 
 type Orders = ReadonlyArray<Order>;
 
-const addNewOrder = (type: OrderType) => {
-	return (orders: Orders) => {
-		const id = orders.length + 1;
+const addNewOrder = (type: OrderType, orders: Orders) => {
+	const id = orders.length + 1;
 
-		const newOrder = {
-			type,
-			id,
-			state: 'PENDING',
-		} satisfies Order;
+	const newOrder = {
+		type,
+		id,
+		state: 'PENDING',
+	} satisfies Order;
 
-		switch (type) {
-			case 'Normal': {
+	switch (type) {
+		case 'Normal': {
+			return orders.concat(newOrder);
+		}
+		case 'VIP': {
+			const firstPendingNormalOrderIndex = orders.findIndex(
+				(order) => order.type === 'Normal' && order.state === 'PENDING'
+			);
+
+			if (firstPendingNormalOrderIndex === -1) {
 				return orders.concat(newOrder);
 			}
-			case 'VIP': {
-				const firstPendingNormalOrderIndex = orders.findIndex(
-					(order) =>
-						order.type === 'Normal' && order.state === 'PENDING'
-				);
 
-				if (firstPendingNormalOrderIndex === -1) {
-					return orders.concat(newOrder);
-				}
+			const beforeOrders = orders.slice(0, firstPendingNormalOrderIndex);
 
-				const beforeOrders = orders.slice(
-					0,
-					firstPendingNormalOrderIndex
-				);
+			const afterOrders = orders.slice(firstPendingNormalOrderIndex);
 
-				const afterOrders = orders.slice(firstPendingNormalOrderIndex);
-
-				return beforeOrders.concat(newOrder, afterOrders);
-			}
+			return beforeOrders.concat(newOrder, afterOrders);
 		}
-	};
+	}
 };
 
-const unprocessOrderById = (orderId: undefined | number) => {
+const unprocessOrderById = (orderId: number) => {
 	return (orders: Orders) => {
 		return orders.map((order) => {
 			return order.id !== orderId
 				? order
 				: ({
-						...order,
+						id: order.id,
+						type: order.type,
 						state: 'PENDING',
-						assignedBotId: undefined,
-					} satisfies Order);
+					} satisfies PendingOrCompleteOrder);
 		});
 	};
 };
 
 export { addNewOrder, unprocessOrderById };
-export type { Order, Orders, OrderType, OrderState };
+export type { Orders, OrderType, ProcessingOrder };
--- test/unit/order/index.test.ts
+++ test/unit/order/index.test.ts
@@ -1,44 +1,67 @@
 import { describe, expect, it } from 'vitest';
 
-import { addNewOrder, type Orders } from '../../../src/order/util';
+import {
+	addNewOrder,
+	unprocessOrderById,
+	type Orders,
+	type ProcessingOrder,
+} from '../../../src/order/util';
 
 describe('Order util functions', () => {
 	it('should add all new normal order to the end of the orders', () => {
-		const incomingOrders = [
+		const orders = (['Normal', 'Normal'] as const).reduce(
+			(orders, type) => {
+				return addNewOrder(type, orders);
+			},
+			[] as Orders
+		);
+
+		expect(orders).toStrictEqual([
 			{ id: 1, type: 'Normal', state: 'PENDING' },
 			{ id: 2, type: 'Normal', state: 'PENDING' },
-		] satisfies Orders;
-
-		const orders = incomingOrders.reduce((orders, newOrder) => {
-			return addNewOrder(newOrder.type)(orders);
-		}, [] as Orders);
-
-		expect(orders).toStrictEqual(incomingOrders);
+		]);
 	});
 
 	it('should add new VIP order ahead of all normal orders', () => {
-		const incomingOrders = [
-			{ id: 1, type: 'Normal', state: 'PENDING' },
-			{ id: 2, type: 'VIP', state: 'PENDING' },
-		] satisfies Orders;
-
-		const orders = incomingOrders.reduce((orders, newOrder) => {
-			return addNewOrder(newOrder.type)(orders);
+		const orders = (['Normal', 'VIP'] as const).reduce((orders, type) => {
+			return addNewOrder(type, orders);
 		}, [] as Orders);
 
-		expect(orders).toStrictEqual(incomingOrders.reverse());
+		expect(orders).toStrictEqual([
+			{ id: 2, type: 'VIP', state: 'PENDING' },
+			{ id: 1, type: 'Normal', state: 'PENDING' },
+		]);
 	});
 
 	it('should add all new VIP order to the end of the orders', () => {
-		const incomingOrders = [
-			{ id: 1, type: 'VIP', state: 'PENDING' },
-			{ id: 2, type: 'VIP', state: 'PENDING' },
-		] satisfies Orders;
-
-		const orders = incomingOrders.reduce((orders, newOrder) => {
-			return addNewOrder(newOrder.type)(orders);
+		const orders = (['VIP', 'VIP'] as const).reduce((orders, type) => {
+			return addNewOrder(type, orders);
 		}, [] as Orders);
 
-		expect(orders).toStrictEqual(incomingOrders);
+		expect(orders).toStrictEqual([
+			{ id: 1, type: 'VIP', state: 'PENDING' },
+			{ id: 2, type: 'VIP', state: 'PENDING' },
+		]);
+	});
+
+	it('should unprocess a processing order by its id', () => {
+		const orders = (['Normal', 'VIP'] as const).reduce((orders, type) => {
+			return addNewOrder(type, orders);
+		}, [] as Orders);
+
+		const processingOrders = orders.map((order) => {
+			return {
+				...order,
+				state: 'PROCESSING',
+				assignedBotId: order.id,
+			} satisfies ProcessingOrder;
+		});
+
+		const updatedOrders = unprocessOrderById(2)(processingOrders);
+
+		expect(updatedOrders).toStrictEqual([
+			{ id: 2, type: 'VIP', state: 'PENDING' },
+			{ id: 1, type: 'Normal', state: 'PROCESSING', assignedBotId: 1 },
+		]);
 	});
 });
