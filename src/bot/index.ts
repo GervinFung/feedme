d20bbb87d8ffef036dca23bb9228df7dec9113cb
GervinFung <gervinfungdaxuen@gmail.com>
2025-09-13 15:41:02 +0800

    bot(util): create function that add and remove bot

---
+++ src/bot/index.ts
@@ -0,0 +1,36 @@
+type Bot = Readonly<{
+	id: number;
+	// remove isIdle bcoz derivable
+	isIdle: boolean;
+	processingOrderId?: number;
+	timer?: ReturnType<typeof setTimeout>;
+}>;
+
+type Bots = ReadonlyArray<Bot>;
+
+const addNewBot = (bots: Bots) => {
+	return bots.concat({
+		id: bots.length + 1,
+		isIdle: true,
+	});
+};
+
+const removeLatestBot = (bots: Bots) => {
+	const botToRemove = bots.at(-1);
+
+	if (botToRemove?.processingOrderId) {
+		return {
+			isBotProcessingOrder: true,
+			botToRemove,
+			bots: bots.slice(0, -1),
+		} as const;
+	}
+
+	return {
+		isBotProcessingOrder: false,
+		bots: bots.slice(0, -1),
+	} as const;
+};
+
+export { addNewBot, removeLatestBot };
+export type { Bot, Bots };
---
+++ test/unit/bot/index.test.ts
@@ -0,0 +1,53 @@
+import { describe, expect, it } from 'vitest';
+
+import { addNewBot, removeLatestBot, type Bots } from '../../../src/bot';
+
+describe('Bot util functions', () => {
+	it('should add new bot to bot list', () => {
+		const incomingBots = [
+			{ id: 1, isIdle: true },
+			{ id: 2, isIdle: true },
+		] satisfies Bots;
+
+		const bots = incomingBots.reduce(addNewBot, [] as Bots);
+
+		expect(bots).toStrictEqual(incomingBots);
+	});
+
+	it('should remove latest bot from bot list and return indicator that bot is not processing any order', () => {
+		const incomingBots = [
+			{ id: 1, isIdle: true },
+			{ id: 2, isIdle: true },
+		] satisfies Bots;
+
+		const bots = incomingBots.reduce(addNewBot, [] as Bots).map((bot) => {
+			if (bot.id === 1) {
+				return bot;
+			}
+
+			return {
+				...bot,
+				isIdle: true,
+				processingOrderId: 1,
+			};
+		});
+
+		const bot2 = removeLatestBot(bots);
+
+		expect(bot2).toStrictEqual({
+			isBotProcessingOrder: true,
+			botToRemove: {
+				...incomingBots.at(1),
+				processingOrderId: 1,
+			},
+			bots: [incomingBots.at(0)],
+		});
+
+		const bot1 = removeLatestBot(bot2.bots);
+
+		expect(bot1).toStrictEqual({
+			isBotProcessingOrder: false,
+			bots: [],
+		});
+	});
+});
