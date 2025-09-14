bcd55dc241218a2e97f228405ecbb0f473f88cf6
GervinFung <gervinfungdaxuen@gmail.com>
2025-09-14 14:51:46 +0800

    order(list): reduce redundant code

--- src/order/list.tsx
+++ src/order/list.tsx
@@ -1,19 +1,23 @@
-import type { Orders } from './util';
+import type { Order, Orders } from './util';
 
 const OrderList = (
 	props: Readonly<{
-		title: string;
+		title: Order['state'];
 		orders: Orders;
 	}>
 ) => {
+	const orders = props.orders.filter((order) => {
+		return order.state === props.title;
+	});
+
 	return (
 		<div>
 			<h3>{props.title}</h3>
-			{!props.orders.length ? (
+			{!orders.length ? (
 				<p style={{ color: '#888' }}>No orders</p>
 			) : (
 				<ul style={{ listStyle: 'none', padding: 0 }}>
-					{props.orders.map((order) => {
+					{orders.map((order) => {
 						return (
 							<li key={order.id}>
 								[{order.type}] Order #{order.id}
@@ -36,24 +40,9 @@ const OrderByCategory = (
 ) => {
 	return (
 		<div style={{ display: 'flex', gap: 32 }}>
-			<OrderList
-				orders={props.orders.filter((order) => {
-					return order.state === 'PENDING';
-				})}
-				title="PENDING"
-			/>
-			<OrderList
-				orders={props.orders.filter((order) => {
-					return order.state === 'PROCESSING';
-				})}
-				title="PROCESSING"
-			/>
-			<OrderList
-				orders={props.orders.filter((order) => {
-					return order.state === 'COMPLETE';
-				})}
-				title="COMPLETE"
-			/>
+			<OrderList orders={props.orders} title="PENDING" />
+			<OrderList orders={props.orders} title="PROCESSING" />
+			<OrderList orders={props.orders} title="COMPLETE" />
 		</div>
 	);
 };
